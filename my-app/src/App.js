f06658a7282e4d42950acfd7519df60e0551d7f7
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2024-12-18 16:40:45 -0500

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my App</h1>
+      <MyButton/>
+    </div>
+  );
+}
+
+function MyButton() {
+  return (
+    <button>Click me!</button>
+  )
+}
+
+export default App;
