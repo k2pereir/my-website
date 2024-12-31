bede484237524b4e6c2de1fbc2f5e9ddc9aaea35
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2024-12-31 16:52:15 -0500

    image carousel added to about-me section

--- my-app/src/components/about-me.js
+++ my-app/src/components/about-me.js
@@ -1,24 +1,72 @@
 import React from 'react';
 import './about-me.css';
-import me from './images/temp_me.jpg'; 
+import me from './images/temp_me.jpg';
+import home from './images/temp.png';
+import sign from './images/sign.png';
+import {useState} from 'react';
+
 
 const AboutMe = () => {
+    const [musicIndex, setMusicIndex] = useState(0);
+    const [cookingIndex, setCookingIndex] = useState(0);
+    const [readingIndex, setReadingIndex] = useState(0);
+    const [travelingIndex, setTravelingIndex] = useState(0);
+
+    const musicImages = [me, home, sign];
+    const cookingImages = [me, home, sign];
+    const readingImages = [me, home, sign];
+    const travelingImages = [me, home, sign];
+
+    const handleImageChange = (setIndex, index, totalImages) => {
+        console.log(`Current index: ${index}, Total images: ${totalImages}`);
+        setIndex(prevIndex => {
+            const newIndex = prevIndex === totalImages - 1 ? 0 : prevIndex + 1;
+            console.log(`New index: ${newIndex}`); 
+            return newIndex;
+        });
+    };
+    
+;
     return (
         <section id="aboutme">
             <div className="aboutme">
                 <p className='intro'>Beyond my academic and professional pursuits, I try to maintain a healthy work-school-life balance through hobbies</p>
                 <p className='phrase'>For me, this means <b>music, cooking, reading, and traveling</b>!</p>
                 <div className='boxes'>
-                    <img src={me} alt='me' className='box box-music'/>
-                    <img src={me} alt='me' className='box box-cooking'/>
-                    <img src={me} alt='me' className='box box-reading'/>
-                    <img src={me} alt='me' className='box box-traveling'/>
+                    <div className='box box-music'>
+                        <img src={musicImages[musicIndex]} alt='music' className='carousel-image'/>
+                        <div className='carousel-buttons'>
+                            <button className='arrow-left' onClick={() => handleImageChange(setMusicIndex, musicIndex, musicImages.length)}>&#8592;</button>
+                            <button className='arrow-right' onClick={() => handleImageChange(setMusicIndex, musicIndex, musicImages.length)}>&#8594;</button>
+                        </div>
+                    </div>
+                    <div className='box box-cooking'>
+                        <img src={cookingImages[cookingIndex]} alt='cooking' className='carousel-image'/>
+                        <div className='carousel-buttons'>
+                            <button className='arrow-left' onClick={() => handleImageChange(setCookingIndex, cookingIndex, cookingImages.length)}>&#8592;</button>
+                            <button className='arrow-right' onClick={() => handleImageChange(setCookingIndex, cookingIndex, cookingImages.length)}>&#8594;</button>
+                        </div>
+                    </div>
+                    <div className='box box-reading'>
+                        <img src={readingImages[readingIndex]} alt='reading' className='carousel-image'/>
+                        <div className='carousel-buttons'>
+                            <button className='arrow-left' onClick={() => handleImageChange(setReadingIndex, readingIndex, readingImages.length)}>&#8592;</button>
+                            <button className='arrow-right' onClick={() => handleImageChange(setReadingIndex, readingIndex, readingImages.length)}>&#8594;</button>
+                        </div>
+                    </div>
+                    <div className='box box-traveling'>
+                        <img src={travelingImages[travelingIndex]} alt='traveling' className='carousel-image'/>
+                        <div className='carousel-buttons'>
+                            <button className='arrow-left' onClick={() => handleImageChange(setTravelingIndex, travelingIndex, travelingImages.length)}>&#8592;</button>
+                            <button className='arrow-right' onClick={() => handleImageChange(setTravelingIndex, travelingIndex, travelingImages.length)}>&#8594;</button>
+                        </div>
+                    </div> 
                 </div>
-                <div className='arrows'>
-                    <div className='arrow arrow-music'></div>
-                    <div className='arrow arrow-cooking'></div>
-                    <div className='arrow arrow-reading'></div>
-                    <div className='arrow arrow-traveling'></div>
+                <div className='pointers'>
+                    <div className='point point-music'></div>
+                    <div className='point point-cooking'></div>
+                    <div className='point point-reading'></div>
+                    <div className='point point-traveling'></div>
                 </div>
                 <div className="music-vertical-1"></div>
                 <hr className='music-horizontal'/>
@@ -32,9 +80,9 @@ const AboutMe = () => {
                 <div className="traveling-vertical-1"></div>
                 <hr className='traveling-horizontal'/>
                 <div className="traveling-vertical-2"></div>
-            </div>
+            </div> 
         </section>  
     )
 }
 
-export default AboutMe; 
+export default AboutMe;
