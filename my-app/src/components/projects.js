5804e78a8ac7965930696cf82ef61e1ce75addcb
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2025-01-06 12:48:24 -0500

    updated project descriptions

--- my-app/src/components/projects.js
+++ my-app/src/components/projects.js
@@ -19,7 +19,7 @@ const Projects = () => {
         <p className='name'>Personal Website</p>
         <a className='link' href="https://github.com/k2pereir/my-website">Follow along here!</a>
         <img className='image' src={personalWeb} alt='temp'></img>
-        <p className='description'>Created with React and deployed with GitHub Pages. This website is a showcase of my projects and skills</p>
+        <p className='description'>This website was built using React, with the backend developed in JavaScript and the frontend styled with CSS. It was deployed using GitHub Pages and serves as a showcase of my projects and skill sets.</p>
       </div>; 
     } else if (selectedProject === 'SignQuest') {
       return <div className='container'>
@@ -27,7 +27,7 @@ const Projects = () => {
         <a className='link' href="https://youtu.be/KXfTq8JxxY8">Watch the pitch video!</a>
         <a className='link2' href="https://github.com/ohi-711/jamhacks8project">Take a look at the code!</a>
         <img className='image' src={signQuest} alt='singQuest'></img>
-        <p className='description'>Created with Python, HTML, and CSS, this project is a sign language learning game that implements the MediaPipe Hand Landmarker task and OpenCV</p>
+        <p className='description'>SignQuest is a web application with a Python backend that generates a word from a predetermined list, prompting the user to demonstrate its sign language representation. It uses OpenCV to access the camera and MediaPipe's Hand Landmarking to verify the correctness of the sign. The frontend was developed using HTML and CSS.</p>
       </div>;
     } else if (selectedProject === 'Sewstainability') {
       return <div className='container'>
@@ -35,28 +35,28 @@ const Projects = () => {
       <a className='link' href="https://youtu.be/_IGU8IxzBbA">Watch the demo video!</a>
       <a className='link2' href='https://github.com/k2pereir/technova_project'>Take a look at the code!</a>
       <img className='image' src={sewstain} alt='sewstainability'></img>
-      <p className='description'>Created with Python and Streamlit, this projects promotes sustainable fashion with an online closet, thrift store, and outfit-repeat tracker</p>
+      <p className='description'>Sewstainability was created with Python and Streamlit for frontend styling. It promotes sustainable fashion with an online closet which users can track how often they reuse pieces, a thrift store where users can sell items they no longer use, and a virtual garden to incentivise the repetition of outfits.</p>
     </div>;
     } else if (selectedProject === 'Rock, Paper, Scissors') {
       return <div className='container'>
       <p className='name'>Rock, Paper, Scissors</p>
       <a className='link' href="https://github.com/k2pereir/rockpaperscissors">Follow along here!</a>
       <img className='image' src={rps} alt='rock, paper, scissors'></img>
-      <p className='description'>My current project is in Python and uses the MediaPipe Hand Landmarking and OpenCV to create a single player rock paper scissors game</p>
+      <p className='description'>My current project uses OpenCV to access the camera and collect information through MediaPipe Hand Landmarking on hand positions for rock, paper, and scissors. Data augmentation techniques such as scaling, flipping, and rotating are applied to extend the dataset and will be used to train a model to recognize rock, paper, and scissor gestures to create a single player version of the game.</p>
     </div>;
     } else if (selectedProject === 'Financial Tracker') {
       return <div className='container'>
       <p className='name'>Financial Tracker</p>
       <a className='link' href="https://youtu.be/S5iEzmtLn5A">Watch the demo video!</a>
-      <img className='image' src={financialtracker} alt='financial tracker'></img>
-      <p className='description'>Made with Excel Macros and VBA, this project is a comprehensive financial tracker</p>
+      <img className='image1' src={financialtracker} alt='financial tracker'></img>
+      <p className='description'>This financial tracker was made with Excel Macros and VBA. It contains a income and expense tracker, comprehensive breakdown of spendings, predictions for future spendings, a virtual savings jar with the ability to track savings goals, and a financial advice feature with hyperlinks to resources based on the user’s current financial state.</p>
     </div>;
     } else if (selectedProject === 'Varos') {
       return <div className='container'>
       <p className='name'>Varos</p>
       <a className='link' href="https://github.com/milkman9999/jamhaxors1">Take a look at the code!</a>
       <img className='image' src={varos} alt='varos'></img>
-      <p className='description'>'Made with JavaScript, the OpenAI API, HTML, and CSS, this web application generates a custom workout and diet plan based on user input</p>
+      <p className='description'>Made with JavaScript, the OpenAI API, HTML, and CSS, Varos generates a custom workout and diet plan based on user inputted information.</p>
     </div>;
     }
   }
@@ -117,15 +117,15 @@ const Projects = () => {
             style={{
               position: 'absolute',
               border: '2px',
-              borderColor: 'rgb(113, 113, 113)', 
+              borderColor: 'rgb(249, 217, 241)', 
               width: '40rem', 
-              height: '30rem',
+              height: '35rem',
               backgroundColor: 'rgb(113, 113, 113)',
-              marginTop: '-250px',
+              marginTop: '-260px',
               marginLeft: '250px',
               borderRadius: '10px',
-              borderColor: 'rgb(33, 34, 34)',
-              boxShadow: '0 4px 8px rgb(113, 113, 113)',
+              borderColor: 'rgb(249, 217, 241)',
+              boxShadow: '0 4px 8px rgb(249, 217, 241)',
               color: 'rgb(33, 34, 34)',
               zIndex: 9999,
               }}>
